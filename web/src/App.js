cdc156d47d7fba925dcb6fd626d9efb2d44c21ed
iyc-wasin <wasin.passornpakorn@ifyoucan.com>
2019-07-12 02:20:21 +0700

    test with Snowtooth

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,100 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Signup from "./components/summary/summary";
+import styled from "styled-components";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
-function App() {
+const ALL_LIFTS_QUERY = gql`
+  query {
+    allLifts {
+      id
+      name
+      status
+    }
+  }
+`;
+const MUTATION = gql`
+  mutation SetLiftStatus($id: ID!, $status: LiftStatus!) {
+    setLiftStatus(id: $id, status: $status) {
+      id
+      status
+    }
+  }
+`;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Signup />
+//     </div>
+//   );
+// }
+// export default App;
+
+export default function App() {
+  const { loading, data } = useQuery(ALL_LIFTS_QUERY);
+  const [setLiftStatus] = useMutation(MUTATION);
+
+  if (loading) return <p>loading lifts</p>;
   return (
-    <div className="App">
-      <Signup />
-    </div>
+    <section>
+      <h1>Snowtooth Lift Status</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Lift Name</th>
+            <th>Current Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allLifts.map(lift => (
+            <tr key={lift.id}>
+              <td>{lift.name}</td>
+              <td>
+                {lift.status}
+                <StatusIndicator
+                  status={lift.status}
+                  onChange={status => {
+                    const variables = { id: lift.id, status };
+                    setLiftStatus({ variables });
+                  }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 }
 
-export default App;
+const StatusIndicator = ({ status = "CLOSED", onChange = f => f }) => (
+  <>
+    <Circle
+      color="green"
+      selected={status === "OPEN"}
+      onClick={() => onChange("OPEN")}
+    />
+    <Circle
+      color="yellow"
+      selected={status === "HOLD"}
+      onClick={() => onChange("HOLD")}
+    />
+    <Circle
+      color="red"
+      selected={status === "CLOSED"}
+      onClick={() => onChange("CLOSED")}
+    />
+  </>
+);
+const Circle = styled.div`
+  border-radius: 50%;
+  background-color: ${({ color, selected }) =>
+    selected ? color : "transparent"};
+  border: solid 2px ${({ color }) => color};
+  border-width: ${({ selected }) => (selected ? "0" : "2")};
+  width: 20px;
+  height: 20px;
+  float: left;
+  cursor: pointer;
+  margin: 0 4px;
+`;
--- web/src/index.js
+++ web/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { render } from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const link = createHttpLink({ uri: "http://snowtooth.moonhighway.com" });
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
